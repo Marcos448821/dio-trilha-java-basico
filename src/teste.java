c90c6f74fc6d6db3a1df044de8058d536dd8c630
marco <marcosdasilva02025@hotmail.com>
2024-10-22 17:43:51 -0300

    2alteracao

--- src/teste.java
+++ src/teste.java
@@ -1,5 +1,5 @@
 public class teste {
       public static void main(String[] args) {
-            System.out.println("maisumtesteqlqcoisaapaga");
+            System.out.println("Na Vida Tem Que Ter Uma Pesistencia para Conquista O Objetivo");
       }
 }
